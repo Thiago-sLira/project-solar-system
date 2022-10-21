af09033047124b5dc974d1881389268f77e29b0e
Thiago Lira <thiago.slira@outlook.com>
2022-10-21 16:15:40 -0300

    Requisito 7 concluído

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './App.css';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import Missions from './components/Missions';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="App">
         <Header />
         <SolarSystem />
+        <Missions />
       </div>
     );
   }
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -6,12 +6,11 @@ import Planets from '../data/planets';
 class SolarSystem extends Component {
   render() {
     const planetas = 'Planetas';
-    const planets = Planets;
     return (
       <div data-testid="solar-system">
         <Title headline={ planetas } />
         <div>
-          {planets.map(({ name, image }, index) => (
+          {Planets.map(({ name, image }, index) => (
             <PlanetCard key={ index } planetName={ name } planetImage={ image } />
           ))}
         </div>
