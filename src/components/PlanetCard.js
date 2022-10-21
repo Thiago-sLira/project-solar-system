1a6de1f4c750fb7994e45cac4a119376aeb56e6b
Thiago Lira <thiago.slira@outlook.com>
2022-10-21 15:47:37 -0300

    Requisito 5 concluído

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">
+          { planetName }
+        </p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.defaultProps = {
+  planetName: 'Falta o nome do planeta',
+  planetImage: 'Falta o link da imagem',
+};
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string,
+  planetImage: PropTypes.string,
+};
+
+export default PlanetCard;
